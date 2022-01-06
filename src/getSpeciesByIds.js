68d3aa9816e5a39c5c3176c39d349619bf330e97
guthias <gmgustavom@gmail.com>
2022-01-06 14:54:28 -0300

    Retornando uma unica especie getSpeciesByID

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-
+  const species = [];
+  species.push(data.species.find((specie) => specie.id === ids));
+  return species;
 }
 
 module.exports = getSpeciesByIds;
