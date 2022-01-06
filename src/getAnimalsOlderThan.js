112ee7653e73b9af7e98c6870c45752410d58bce
guthias <gmgustavom@gmail.com>
2022-01-06 15:12:22 -0300

    Verificando se todos os animais tem a idade minima

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = data.species.find((specie) => specie.name === animal);
+
+  return animals.residents.every((specie) => specie.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
