676f4b6fc38480fdaa6040efda66b4d6cc90baad
guthias <gmgustavom@gmail.com>
2022-01-06 15:13:10 -0300

    Refatorando a function getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const animals = data.species.find((specie) => specie.name === animal);
-
-  return animals.residents.every((specie) => specie.age > age);
+  return data.species.find((specie) => specie.name === animal)
+    .residents.every((specie) => specie.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
