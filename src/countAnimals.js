0eb6514157f4daeb6e7b99f2f590c420f3652c68
guthias <gmgustavom@gmail.com>
2022-01-06 18:03:03 -0300

    Filtrando especies para a contagem dos animais

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -10,6 +10,13 @@ function countAllAnimals() {
 
 function countAnimals(animal) {
   if (!animal) return countAllAnimals();
+
+  const animalFromSpecie = data.species.find((specie) => specie.name === animal.specie);
+  // if (animal.species && animal.sex) {
+  //   return animalFromSpecie.residents.filter((resident) => resident.sex === animal.sex).length;
+  // }
+
+  return animalFromSpecie.residents.length;
 }
 
 console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
