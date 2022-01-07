9087d3073e5bcd3ece53346c399f2e9861823bf8
guthias <gmgustavom@gmail.com>
2022-01-07 16:44:47 -0300

    Horario de abertura para todos os dias

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const openTime = (open, close) => {
+  if (open === 0) return 'CLOSED';
+  return `Open from ${open}am until ${close}pm`;
+};
+
+const exhibition = (weekDay) => {
+
+};
+
+function fullSchedule() {
+  const schedule = {};
+  Object.keys(data.hours).forEach((weekDay) => {
+    schedule[weekDay] = {
+      officeHour: openTime(weekDay.open, weekDay.close),
+      exhibition: exhibition(weekDay) };
+  });
+
+  return schedule;
 }
+
+function getSchedule(scheduleTarget) {
+  return fullSchedule();
+}
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
