8638b5bc152601c01d4f562886d8c93d7feb400c
guthias <gmgustavom@gmail.com>
2022-01-07 18:19:40 -0300

    Formatando dados do animal mais velho

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,11 @@
 const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  return species.find((specie) => specie.id === employees
+  const oldestAnimal = species.find((specie) => specie.id === employees
     .find((employee) => employee.id === id).responsibleFor[0]).residents
     .sort((a, b) => b.age - a.age)[0];
+
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
