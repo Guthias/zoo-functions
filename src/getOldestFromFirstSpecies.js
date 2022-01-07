d5a5ee3aa6850945d6ee5076646c7375fac05034
guthias <gmgustavom@gmail.com>
2022-01-07 18:10:44 -0300

    Retornando os primeiros animais que a pessoa é responsavel

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  return species.find((specie) => specie.id === employees
+    .find((employee) => employee.id === id).responsibleFor[0]).residents;
 }
 
+console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 module.exports = getOldestFromFirstSpecies;
