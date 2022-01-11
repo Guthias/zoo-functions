89e9edb38b8a5d6cfaebcebe12d4e91a620ce4b5
unknown <gmgustavom@gmail.com>
2022-01-10 21:22:27 -0300

    Refatorando getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,5 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  const species = [];
-  ids.forEach((id) => species.push(data.species.find((specie) => specie.id === id)));
-  return species;
-}
+const getSpeciesByIds = (...ids) => ids.map((id) => species.find((specie) => specie.id === id));
 
 module.exports = getSpeciesByIds;
