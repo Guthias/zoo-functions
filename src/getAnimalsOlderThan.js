da0fba3d489a24116abeffce996ebe42a8c3ef79
unknown <gmgustavom@gmail.com>
2022-01-10 21:35:33 -0300

    Refatorando getAnimalsOlderTHan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,6 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  return data.species.find((specie) => specie.name === animal)
-    .residents.every((specie) => specie.age > age);
-}
+const getAnimalsOlderThan = (animal, age) => species.find((specie) => specie.name === animal)
+  .residents.every((specie) => specie.age > age);
 
 module.exports = getAnimalsOlderThan;
