ebe47ea729cb932e8d3d14285c045980288b070a
unknown <gmgustavom@gmail.com>
2022-01-11 20:49:16 -0300

    Refatorando animalsIncludesNames

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -10,23 +10,12 @@ const filterBySex = (sex, specie) => specie.residents.reduce((acc, resident) => 
   return acc;
 }, []);
 
-const animalsIncludeNames = (sex = undefined, sorted = false) => {
-  const animalMap = { NE: [], NW: [], SE: [], SW: [] };
-
-  let animalNames;
-  species.forEach((specie) => {
-    if (sex) {
-      animalNames = filterBySex(sex, specie);
-    } else {
-      animalNames = specie.residents.map(({ name }) => name);
-    }
-
-    if (sorted) animalNames = animalNames.sort();
-    animalMap[specie.location].push({ [specie.name]: animalNames });
-  });
-
-  return animalMap;
-};
+const animalsIncludeNames = (sex = undefined, sorted = false) => species.reduce((acc, specie) => {
+  let animalNames = (sex) ? filterBySex(sex, specie) : specie.residents.map(({ name }) => name);
+  if (sorted) animalNames = animalNames.sort();
+  acc[specie.location].push({ [specie.name]: animalNames });
+  return acc;
+}, { NE: [], NW: [], SE: [], SW: [] });
 
 function getAnimalMap(options = {}) {
   return options.includeNames
