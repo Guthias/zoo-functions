f4f4a8c3d79eb80831914a008c33af48ea1164c2
unknown <gmgustavom@gmail.com>
2022-01-11 21:06:23 -0300

    Refatorando getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,11 +1,8 @@
 const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  const oldestAnimal = species.find((specie) => specie.id === employees
+const getOldestFromFirstSpecies = (id) => Object.values(species
+  .find((specie) => specie.id === employees
     .find((employee) => employee.id === id).responsibleFor[0]).residents
-    .sort((a, b) => b.age - a.age)[0];
-
-  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
-}
+  .sort((a, b) => b.age - a.age)[0]);
 
 module.exports = getOldestFromFirstSpecies;
